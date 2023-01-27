800f24ec1efbfc5e45ce7ffb374328a380b789dc
Patrick Esmerado <89804840+SSgtE@users.noreply.github.com>
2023-01-27 11:04:18 -0500

    First Commit
    Changed the O to an I in smooth position to allow unity to open up without safe mode

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,7 +10,7 @@ public class CameraFollow : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosotion = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
 
         transform.LookAt(target);

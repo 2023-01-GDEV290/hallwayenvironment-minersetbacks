f44bb043a4c830c03261ec96fbdd7ea503251016
SSgtE <89804840+SSgtE@users.noreply.github.com>
2023-01-29 19:19:02 -0500

    imported the miner and things.
    I hope this worked

---
+++ Assets/Patrick/Scripts/O2Script.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class O2Script : MonoBehaviour
+{
+
+    public float maxOxy = 180.0f;
+    private float CurrentOxy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentOxy = maxOxy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentOxy -= Time.deltaTime;
+
+        if (CurrentOxy <= 0.0f)
+        {
+            UnityEngine.Debug.Log("Youre Dead");
+        }
+    }
+}
+
+
+
---
+++ Assets/Patrick/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Although this script is named Player, DO NOT mistaken it or the Third-person Controller Script!
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            OxygenStamina.instance.UseStamina(10);
+    }
+}

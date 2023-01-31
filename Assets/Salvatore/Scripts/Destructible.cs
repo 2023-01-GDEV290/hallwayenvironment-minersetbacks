ab0d9600b2b2cc71bc08064303f289e39900bd01
sfanzalone <sfanzalone@gmail.com>
2023-01-31 12:55:15 -0500

    Edited Destructible Script & added an opening; everything ran well
    The Destructible Script was edited to support the Swing animation, and the script was compiled with no problems.  Also, a Mineshaft Opening game object was made, as the Destructible Script requires the coder to attach a damaged version of a non-damaged object, for example.

--- Assets/Salvatore/Scripts/Destructible.cs
+++ Assets/Salvatore/Scripts/Destructible.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Destructible : MonoBehaviour
 {
     public GameObject destroyedVersion;
-    //private Animator hit;
+    private Animator Swing;
 
     void onMouseDown()
     {
-        Instantiate(destroyedVersion, transform.position, transform.rotation);
-        Destroy(gameObject);
+        if (Swing)
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
     }
 
     //Code above is duplicated to provide ideal code outline for when an animation for the pickaxe swing is executed.
-    //DO NOT uncomment code, unless we are ready to test it!
+    //DO NOT uncomment code, unless we are ready to test it!  And if need be, use this code as a reference to what
+    //needs to be done.
     //if(hit)
     //{
         //Instantiate(destroyedVersion, transform.position, transform.rotation);

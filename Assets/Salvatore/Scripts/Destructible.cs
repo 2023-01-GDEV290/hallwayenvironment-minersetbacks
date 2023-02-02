d45e31349ce50ff99979491274b75f16252471f3
SSgtE <89804840+SSgtE@users.noreply.github.com>
2023-02-02 09:41:13 -0500

    redoing things
    kthing things

---
+++ Assets/Salvatore/Scripts/Destructible.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    private Animator Swing;
+
+    
+    void onMouseDown()
+    {
+        if (Swing == destroyedVersion)
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    //Code above is duplicated to provide ideal code outline for when an animation for the pickaxe swing is executed.
+    //DO NOT uncomment code, unless we are ready to test it!  And if need be, use this code as a reference to what
+    //needs to be done.
+    //if(hit)
+    //{
+        //Instantiate(destroyedVersion, transform.position, transform.rotation);
+        //Destroy(gameObject);
+    //}
+}
---
+++ Assets/Salvatore/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public float rotationSpeed;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private Animator animator;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        if (direction != Vector3.zero)
+        {
+            transform.forward = direction;
+            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(direction * speed * Time.deltaTime);
+
+            animator.SetBool("IsWalking?", true);
+        }
+
+        else
+        {
+            animator.SetBool("IsWalking?", false);
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            axeSwing();
+        }
+
+        else
+        {
+            animator.SetBool("IsSwinging?", false);
+
+        }
+
+
+    }
+
+
+
+
+    private void axeSwing()
+    {
+        animator.SetBool("IsSwinging?", true);
+    }
+
+
+
+
+}

ef85159626807a53261e7539ddf9546fb08f2191
sfanzalone <sfanzalone@gmail.com>
2023-02-02 12:18:45 -0500

    Script is done; branch is experiencing duplication problems
    The script has been remade, but the branch is undergoing duplication problems.  Hopefully, this doesn't lead to more problems when reentering branch.

--- Assets/Scripts/OxygenStamina.cs
+++ Assets/Scripts/OxygenStamina.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OxygenStamina : MonoBehaviour
 {
+
+    public Slider oxygenStamina;
+
+    private int maxStamina = 100;
+    private int currentStamina;
+
+    public static OxygenStamina instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentStamina = maxStamina;
+        oxygenStamina.maxValue = maxStamina;
+        oxygenStamina.value = maxStamina;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UseStamina(int amount)
     {
-        
+        if(currentStamina - amount >= 0)
+        {
+            currentStamina -= amount;
+            oxygenStamina.value = currentStamina;
+        }
+
+        else
+        {
+            Debug.Log("You have 30 seconds left before you die");
+        }
     }
 }

26026999c55e247276ca471a05bcd9c9d99aaa01
sfanzalone <sfanzalone@gmail.com>
2023-02-09 14:45:04 -0500

    Revert "Added scripts aid win & die conditions"
    This reverts commit 6e049b46c84dba419d56346d9f45fbebbd636377.

--- Assets/Salvatore/Scripts/WinDie.cs
+++ Assets/Salvatore/Scripts/WinDie.cs
@@ -1,28 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+//Massive thanks to Professor Michael Klein for allowing me to
+//Download a project of his for inspiration & understanding for a past project!
+//Project's name: H'SUB DINNER
+//Other Projects that used code: SSB Inspired Fighting Game
 
 public class WinDie : MonoBehaviour
 {
-    private bool gameEnded;
-    public double O2BarValue;
-    
-    public void WinLevel()
+    //public GameObject WinText;
+    //public GameObject LoseText;
+    public float PauseTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        //if (gameEnded && O2BarValue != 0.0)
-        //{
-            Debug.Log("You Win!");
-            gameEnded = true;
-        //}
+        //SaveScript.TimeOut = false;
+        //WinText.gameObject.SetActive(false);
+        //LoseText.gameObject.SetActive(false);
+        //StartCoroutine(WinSet());
     }
 
-    //public void LoseLevel()
-    //{
-        //if (!gameEnded && O2BarValue == 0.0)
-        //{
-            //Debug.Log("You Die!");
-            //gameEnded = true;
-        //}
-    //}
+    IEnumerator WinSet()
+    {
+        yield return new WaitForSeconds(0.4f);
+
+        //if (SaveScript.CurrentOxy >= 0.0f)
+        {
+            //SceneManager.LoadScene(0);
+        }
+
+
+        //if (SaveScript.CurrentOxy > 0.0f)
+        {
+            //WinText.gameObject.SetActive(true);
+            //MyPlayer.Play();
+            //SaveScript.YouWin++;
+        }
+
+        //else if (SaveScript.CurrentOxy == 0.0f)
+        {
+            //LoseText.gameObject.SetActive(true);
+            //MyPlayer.Play();
+            //SaveScript.YouDied++;
+        }
+    }
 }
